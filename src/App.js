0da1090fdcad20f2ddc22c994da9df5c690328e3
MicheleTognin <60986445+MicheleTognin@users.noreply.github.com>
2020-05-30 18:12:14 +0200

    signin

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import './App.css';
 
 function App() {
   return (
-   <div>
-     <Header />
-     <Switch>
-    <Route exact path='/' component={HomePage} />
-    <Route path='/shop' component={ShopPage} />
-     </Switch>
-  </div> 
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SingInAndSignUpPage} />
+      </Switch>
+    </div>
   );
 }
 
