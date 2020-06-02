98a2470fdf18e65acde4d48009a51504baa662c0
MicheleTognin <60986445+MicheleTognin@users.noreply.github.com>
2020-06-02 10:54:47 +0200

    sign in form with google

--- src/App.js
+++ src/App.js
@@ -5,18 +5,42 @@ import ShopPage from './pages/shop/shop.component';
 import SingInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import './App.css';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SingInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unubscribeFromAuth = null;
+
+  componentDidMount() {
+     this.unubscribeFromAuth = auth.onAuthStateChanged((user) => {
+       this.setState({ currentUser: user });
+       console.log(user);
+     });
+  }
+
+  componentWillUnmount() {
+    this.unubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SingInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
